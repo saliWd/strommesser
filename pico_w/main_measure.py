d7bf742a18da68fafaad9b30c11afab3fea0869f
saliWd <57621907+saliWd@users.noreply.github.com>
2022-11-20 10:43:24 +0100

    removed timer stuff

--- pico_w/main_measure.py
+++ pico_w/main_measure.py
@@ -1,14 +1,15 @@
 import network # type: ignore (this is a pylance ignore warning directive)
 import urequests # type: ignore
 from time import sleep
-from machine import Pin, Timer, UART, WDT # type: ignore
+from machine import Pin, UART, WDT # type: ignore
 
 
 # my own files
 import my_config
 from my_functions import debug_wdtFeed, debug_print, debug_sleep, wlan_connect, urlencode, get_randNum_hash
 
-def uart_ir_e350(wdt, DBGCFG:dict, uart_ir):
+
+def uart_ir_e350(DBGCFG:dict, uart_ir, wdt):
     debug_wdtFeed(wdt=wdt, DBGCFG=DBGCFG)
     if(DBGCFG["ir_sim"]):
         return('/LGZ4ZMF100AC.M26\r\n\x02F.F(00)\r\n0.0(          120858)\r\nC.1.0(13647123)\r\nC.1.1(        )\r\n1.8.1(042951.721*kWh)\r\n1.8.2(018609.568*kWh)\r\n2.8.1(000000.302*kWh)\r\n2.8.2(000010.188*kWh)\r\n1.8.0(061561.289*kWh)\r\n2.8.0(000010.490*kWh)\r\n15.8.0(061571.780*kWh)\r\nC.7.0(0008)\r\n32.7(241*V)\r\n52.7(243*V)\r\n72.7(242*V)\r\n31.7(000.35*A)\r\n51.7(000.52*A)\r\n71.7(000.47*A)\r\n82.8.1(0000)\r\n82.8.2(0000)\r\n0.2.0(M26)\r\nC.5.0(0401)\r\n!\r\n\x03\x01')
@@ -32,6 +33,10 @@ def uart_ir_e350(wdt, DBGCFG:dict, uart_ir):
         print('Warning: UART buffer is not empty after two reads')
     debug_wdtFeed(wdt=wdt, DBGCFG=DBGCFG)        
     if ((uart_str_id == None) or (uart_str_values_0 == None) or (uart_str_values_1 == None)):
+        print('Error: uart communication did not work')
+        if (uart_str_id != None): print('error, id='+uart_str_id.decode())
+        if (uart_str_values_0 != None): print('error, uart_str_values_0='+uart_str_values_0.decode())
+        if (uart_str_values_1 != None): print('error, uart_str_values_1='+uart_str_values_1.decode())
         return('uart communication did not work') # still a string, will not be transmitted ()
     else:
         return(uart_str_values_0.decode()+uart_str_values_1.decode())
@@ -59,7 +64,10 @@ def send_message_and_wait_post(wdt, DBGCFG:dict, message:dict, wait_time:int, le
 
 DBGCFG = my_config.get_debug_settings() # debug stuff
 
-wdt = WDT(timeout=8300)  # enable it with a timeout of 8s. NB: maximum timeout is 8.388 seconds
+if DBGCFG["wdt_dis"]:
+    wdt = 0
+else:
+    wdt = WDT(timeout=8300)  # enable it with a timeout of 8s. NB: maximum timeout is 8.388 seconds
 debug_wdtFeed(wdt=wdt, DBGCFG=DBGCFG)
 LOOP_WAIT_TIME = 40
 
@@ -68,19 +76,19 @@ led_onboard = Pin("LED", Pin.OUT)
 enable3v3_pin = Pin(28, Pin.OUT) # solder pin GP28 to '3V3_EN'-pin
 
 # machine specific stuff
-tim = Timer() # no need to specify a number on pico, all SW timers
-uart_ir = UART(0, baudrate=300, bits=7, parity=0, stop=1, tx=Pin(0), rx=Pin(1), timeout=6000)
+# uart_ir = UART(0, baudrate=300, bits=7, parity=0, stop=1, tx=Pin(0), rx=Pin(1), timeout=6000)
+uart_ir = UART(0, baudrate=300, bits=7, parity=0, stop=1, tx=Pin(0), rx=Pin(1))
 
 # normal variables
 wlan_ok = False
 
 ## program starts here
-led_onboard.off()
+led_onboard.on()
 enable3v3_pin.off()
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-sleep(1)
+sleep(3)
 
 device_config = my_config.get_device_config()
 
@@ -88,8 +96,10 @@ while True:
     debug_wdtFeed(wdt=wdt, DBGCFG=DBGCFG)
     enable3v3_pin.on() # power on IR head
     debug_sleep(wdt=wdt, DBGCFG=DBGCFG, time=2) # make sure 3.3V power is stable
-    uart_received_str = uart_ir_e350(wdt=wdt, DBGCFG=DBGCFG, uart_ir=uart_ir) # this takes some seconds
-    # print(uart_received_str)
+    uart_received_str = uart_ir_e350(DBGCFG=DBGCFG, uart_ir=uart_ir, wdt=wdt) # this takes some seconds
+    
+    
+    debug_print(DBGCFG=DBGCFG, text=uart_received_str)
     enable3v3_pin.off() # power down IR head
 
     # basic check, based on string length
@@ -106,8 +116,8 @@ while True:
         ('randNum', randNum_hash['randNum']),
         ('hash', randNum_hash['hash'])
         ])
-    # debug_print(DBGCFG=DBGCFG, text=str(message))
+    debug_print(DBGCFG=DBGCFG, text=str(message))
     debug_wdtFeed(wdt=wdt, DBGCFG=DBGCFG)
-    wlan_connect(wdt=wdt, DBGCFG=DBGCFG, wlan=wlan, tim=tim, led_onboard=led_onboard) # try to connect to the WLAN. Hangs there if no connection can be made
+    wlan_connect(wdt=wdt, DBGCFG=DBGCFG, wlan=wlan, led_onboard=led_onboard, meas=True) # try to connect to the WLAN. Hangs there if no connection can be made
     send_message_and_wait_post(wdt=wdt, DBGCFG=DBGCFG, message=message, wait_time=LOOP_WAIT_TIME, led_onboard=led_onboard) # does not send anything when in simulation
 # end while
