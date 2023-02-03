a94fba1c3933efd3d0a8507703476d65a21deb2e
saliWd <57621907+saliWd@users.noreply.github.com>
2023-02-03 12:52:31 +0100

    corrected weekday calculation

--- web/verbrauch/statistic.php
+++ web/verbrauch/statistic.php
@@ -15,22 +15,28 @@ $dbConn = initialize();
 $userid = getUserid(); // this will get a valid return because if not, the initialize above will already fail (=redirect)
 printBeginOfPage(enableReload:FALSE, timerange:'', site:'statistic.php');
 
+$minusWeekArr = array(1,1,1,1,1,1,1,1); // 0 to 7
+$weekday = (int)(date_create()->format('N')); // N: 1 (for Monday) through 7 (for Sunday)
+for ($i = $weekday - 1; $i < 8; $i++) { // i = 0 .. 7
+  $minusWeekArr[$i] = 0;
+}
 $dailyStrings = array( // maybe: could this be done more nicely?
-  date_create("last Monday 00:00")->format('Y-m-d 00:00:00'),
-  date_create("last Tuesday 00:00")->format('Y-m-d 00:00:00'),
-  date_create("last Wednesday 00:00")->format('Y-m-d 00:00:00'),
-  date_create("last Thursday 00:00")->format('Y-m-d 00:00:00'),
-  date_create("last Friday 00:00")->format('Y-m-d 00:00:00'),  // this is wrong when today is Friday... Kind of logical
-  date_create("last Saturday 00:00")->format('Y-m-d 00:00:00'),
-  date_create("last Sunday 00:00")->format('Y-m-d 00:00:00'),
-  date_create("this Monday 00:00")->format('Y-m-d 00:00:00') // have a additional one
+  date_create('-'.$minusWeekArr[0].' week Monday 00:00')->format('Y-m-d 00:00:00'),
+  date_create('-'.$minusWeekArr[1].' week Tuesday 00:00')->format('Y-m-d 00:00:00'),
+  date_create('-'.$minusWeekArr[2].' week Wednesday 00:00')->format('Y-m-d 00:00:00'),
+  date_create('-'.$minusWeekArr[3].' week Thursday 00:00')->format('Y-m-d 00:00:00'),
+  date_create('-'.$minusWeekArr[4].' week Friday 00:00')->format('Y-m-d 00:00:00'),
+  date_create('-'.$minusWeekArr[5].' week Saturday 00:00')->format('Y-m-d 00:00:00'),
+  date_create('-'.$minusWeekArr[6].' week Sunday 00:00')->format('Y-m-d 00:00:00'),
+  date_create('-'.$minusWeekArr[7].' week Monday 00:00')->format('Y-m-d 00:00:00') // have a additional one
 );
+
 // for some entries, this sql will return the sum of only one line (thin = 24), for others 24 and for the newest ones it returns the sum of lots of entries 
 $val_y = '';
 for ($i = 0; $i < 7; $i++) {
   $sql = 'SELECT SUM(`consDiff`) as `sumConsDiff`, SUM(`zeitDiff`) as `sumZeitDiff` FROM `verbrauch`';
   $sql = $sql. ' WHERE `userid` = "'.$userid.'" AND `zeit` > "'.$dailyStrings[$i].'" AND `zeit` < "'.$dailyStrings[$i+1].'";';
-  echo $sql."<br />";
+  // echo $sql."<br />";
   $result = $dbConn->query($sql); // returns only one row
   $row = $result->fetch_assoc();
   
