e27211fa9c4021a2693b61ee24d9d50e07d00289
saliWd <57621907+saliWd@users.noreply.github.com>
2023-02-07 15:47:39 +0100

    trial with npm. Not yet successful

---
+++ _div/tailwindcss/notYetWorking_tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./../../web/verbrauch/**/*.{html,js,php}', './node_modules/flowbite/**/*.js'],
+  theme: {
+	fontFamily: {
+	  'sans': ['Raleway', 'Helvetica', 'Arial', 'sans-serif'],  
+	},
+    extend: {
+      colors: {
+        diffColor: {
+          300: '#9ae6b4',
+          700: '#2f855a',          
+        },        
+      }
+    },
+  },
+  plugins: [
+	require('flowbite/plugin')
+  ]
+}
---
+++ _div/tailwindcss/run_npx.bat
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./input.css -o ./../../web/verbrauch/strommesser.css --watch --minify
