6c996e357fda9a45a57c881e4acfc4faf77b2cd4
saliWd <57621907+saliWd@users.noreply.github.com>
2023-02-23 10:19:21 +0100

    added link to statistic

--- web/verbrauch/index.php
+++ web/verbrauch/index.php
@@ -196,6 +196,8 @@ echo '<br><br>
 printBarGraph(values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Week, goBack:0), chartId:'WeeklyNow', title:'diese Woche', isIndexPage:TRUE);
 printBarGraph(values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Month, goBack:0), chartId:'MonthlyNow', title:'diesen Monat', isIndexPage:TRUE);
 
+echo '<p>Weitere Infos und Grafiken findest du auf der<a href="statistic.php" class="font-medium text-blue-600 hover:text-blue-700"><svg class="w-6 h-6 inline" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>Statistikseite</a></p>';
+
 ?>
 <br><br>
 </div></body></html>
