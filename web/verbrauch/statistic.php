0caf3910e3516e6c269a08283a019896c17676f3
saliWd <57621907+saliWd@users.noreply.github.com>
2023-02-24 14:56:07 +0100

    added explanation about statistics

--- web/verbrauch/statistic.php
+++ web/verbrauch/statistic.php
@@ -2,14 +2,12 @@
 require_once('functions.php');
 $dbConn = initialize();
 
-
 // shows several bar graphs:
 // - daily consumption this week (starting monday), consumption last week
 // - daily consumption this month (starting 1st of), consumption last month
 // - weekly consumption this year
 // TODO: always: scrollable (select this month or go back to last month etc.)
 // always: displaying the data as I have it. If only two days this month, I display those...
-// bar graph for few items
 
 $userid = getUserid(); // this will get a valid return because if not, the initialize above will already fail (=redirect)
 printBeginOfPage(site:'statistic.php', title:'Statistiken');
@@ -40,14 +38,45 @@ echo '
     </div>
   </div>
 </nav>
-<br><br>';
+<br><br>
+<div class="text-left">
+  <h3 class="font-semibold text-gray-900">Tageswerte pro Woche</h3>
+  <p>Für jeden Wochentag ist der Durchschnittsverbrauch in Watt dargestellt. Ein Durschnittsverbrauch von 1000 Watt enstpricht einem Tagesverbrauch von 24 kWh</p>
+  <p>Gemessen wird von 00:00 bis 23:59 bzw. am aktuellen Tag von 00:00 bis `jetzt`.</p>
+  <p>Mit den Navigationspfeilen kannst du zwischen den Wochen blättern (TODO: noch in Arbeit)</p>
+</div>
+';
+printBarGraph(
+  values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Week, goBack:0), 
+  chartId:'WeeklyNow', 
+  title:'diese Woche',
+  isIndexPage:FALSE
+);
+printBarGraph(
+  values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Week, goBack:1), 
+  chartId:'WeeklyLast', 
+  title:'letzte Woche',
+  isIndexPage:FALSE
+);
 
-printBarGraph(values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Week, goBack:0), chartId:'WeeklyNow', title:'diese Woche');
-printBarGraph(values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Week, goBack:1), chartId:'WeeklyLast', title:'letzte Woche');
-
+echo '
+<div class="text-left mt-4">
+  <h3 class="font-semibold text-gray-900">Tageswerte pro Monat</h3>
+  <p>Für jeden Tag ist der Durchschnittsverbrauch in Watt dargestellt. Ein Durschnittsverbrauch von 1000 Watt enstpricht einem Tagesverbrauch von 24 kWh</p>
+  <p>Gemessen wird von 00:00 bis 23:59 bzw. am aktuellen Tag von 00:00 bis `jetzt`.</p>
+  <p>Mit den Navigationspfeilen kannst du zwischen den Monaten blättern (TODO: noch in Arbeit)</p>
+</div>
+';
 printBarGraph(values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Month, goBack:0), chartId:'MonthlyNow', title:'diesen Monat');
 printBarGraph(values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Month, goBack:1), chartId:'MonthlyLast', title:'letzten Monat');
 
+echo '
+<div class="text-left mt-4">
+  <h3 class="font-semibold text-gray-900">Wochenwerte übers Jahr</h3>
+  <p>Für jeden Woche ist der Durchschnittsverbrauch in Watt dargestellt. Ein Durschnittsverbrauch von 1000 Watt enstpricht einem Wochenverbrauch von 7*24 kWh</p>
+  <p>Gemessen wird von Montag 00:00 bis Sonntag 23:59 bzw. in der aktuellen Woche von Montag 00:00 bis `jetzt`.</p>
+</div>  
+';
 printBarGraph(values:getValues(dbConn:$dbConn, userid:$userid, timerange:EnumTimerange::Year, goBack:0), chartId:'YearlyNow', title:'dieses Jahr');
 ?>
 </div></body></html>
