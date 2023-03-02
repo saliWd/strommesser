1923eed5310097bc206d842123c365b9ce87a434
saliWd <57621907+saliWd@users.noreply.github.com>
2023-03-02 14:27:51 +0100

    (minor) added configs

--- pico_w/my_config.py
+++ pico_w/my_config.py
@@ -1,19 +1,17 @@
-# returns the ssid and the password of the WLAN connection
 def get_wlan_config():
     config_wlan = dict([
         ("ssid","strommesser"),
         ("pw","publicPW")
-        ])
+    ])
     return(config_wlan)
 
 def get_device_config():
     config_device = dict([
         ("userid","3"),
         ("post_key","X6O4wqb339L3w9Fmjl9kPVAkcwXkVhTj")
-        ])
+    ])
     return(config_device)   
 
-# debug settings
 def get_debug_settings():
     debug_settings = dict([
         ("print",True),
