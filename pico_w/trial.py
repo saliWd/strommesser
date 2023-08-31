88d613e75e4ef04a82f60483d5105c46167c1bd1
saliWd <57621907+saliWd@users.noreply.github.com>
2023-08-31 15:38:58 +0200

    (minor) function update in trial script

--- pico_w/trial.py
+++ pico_w/trial.py
@@ -1,5 +1,5 @@
 # start with h = variable, s = 0.5, v = 0.5, a = LedBrightness/255
-def hsva_to_rgb(h:float, s:float, v:float, a:float) -> tuple:    # inputs: values from 0.0 to 1.0. Outputs are integers, range 0 to 255
+def hsva_to_rgb(h:float, s:float, v:float, a:float) -> tuple: # inputs: values from 0.0 to 1.0. Outputs are integers, range 0 to 255
     if s:
         if h == 1.0: h = 0.0
         i = int(h*6.0); f = h*6.0 - i
@@ -17,18 +17,21 @@ def hsva_to_rgb(h:float, s:float, v:float, a:float) -> tuple:    # inputs: value
         if i==5: return (v, w, q)
     else: v = int(255*v); return (v, v, v)
 
-def value_to_rgb(value:int, value_max:int, led_brightness:int)-> list: # goes from red to blue
-    h = float(value) / float(1.4*value_max) # h value makes a 'circle'. This means 0 degree is the same as 360°. -> Need to limit it (but not to 180°, just less than 360)
+def val_to_rgb(val:int, minValCons:int, maxValGen:int, led_brightness:int)-> list: # goes from red to blue
+    # value has a range from -minValCons to +maxValGen. minVal and maxVal are both positive numbers but minVal may be treated as negative
+    val = val + minValCons # bring it to the range 0..(min+max)
+    h = float(val) / float(1.4*(minValCons+maxValGen)) # h value makes a 'circle'. This means 0 degree is the same as 360°. -> Need to limit it (but not to 180°, just less than 360)
     a = float(led_brightness) / float(255)
     return list(hsva_to_rgb(h, 1.0, 1.0, a))
 
+
+
 my_image = [[]] # empty array
-for value in range(0,400,5):
-    my_rgb = value_to_rgb(value=value, value_max=400, led_brightness=255)
+for value in range(-400,4000,100):
+    my_rgb = val_to_rgb(val=value, minValCons=400, maxValGen=4000, led_brightness=255)
     my_image[0].append(my_rgb) # writing it to index 0 to get a [length x 1 x 3] array
-    print(my_rgb)
+    # print(my_rgb)
 
 from matplotlib import pyplot as plt
 plt.imshow(my_image)
-plt.show()    
-    
+plt.show()
