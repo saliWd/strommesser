31f07c6c517b2b85ffdb3ff67444377019633c88
saliWd <57621907+saliWd@users.noreply.github.com>
2025-05-16 13:34:56 +0200

    file rename, trial with mdns

---
+++ pico2w/ap_trial.py
@@ -0,0 +1,47 @@
+# install micropython-mdns package
+
+import network # type: ignore
+import time
+import socket
+
+
+def web_page():
+  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+            <body><h1>StromMesser web page</h1></body>
+            </html>
+         """
+  return html
+
+# issues on this implementation
+# - client must connect to the given ip (e.g. 192.168.4.1). Can I at least define this IP?
+# - mobile might switch to another WLAN as this one does not provide internet connection
+# - ...need to move some stuff into a function... Currently a tech demo.
+
+ssid='strommesser'
+
+ap = network.WLAN(network.AP_IF) # access point
+# not sure what this does... network.hostname('strommesser.local')
+ap.config(essid=ssid, security=0) # open network, no password
+ap.active(True)
+
+while ap.active() == False:
+    pass
+print('AP Mode Is Active, You can Now Connect')
+print('IP Address To Connect to: ' + ap.ifconfig()[0])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creating socket object
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  print('Content = %s' % str(request))
+  response = web_page()
+  conn.send(response)
+  conn.close()
+  time.sleep(3)
+
+
+
