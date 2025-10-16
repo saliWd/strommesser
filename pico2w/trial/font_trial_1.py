6d6b4acd0b41824df84745570d1abf50be5bbadf
saliWd <57621907+saliWd@users.noreply.github.com>
2025-10-16 11:12:20 +0200

    example for the support case

--- pico2w/trial/font_trial_1.py
+++ pico2w/trial/font_trial_1.py
@@ -1,25 +1,25 @@
 #IMPORT LIBRARIES
 from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2 # type: ignore
-from picovector import PicoVector, ANTIALIAS_NONE # type: ignore
+from picovector import PicoVector, ANTIALIAS_X4 # type: ignore
 
 #SET DISPLAY
 display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2)
 display.set_backlight(0.8)
-#SET VECTOR, ANTIALIASING & FONT TYPE {download.af fonts from resources above. Upload through Thonny in root "/" }
-# https://github.com/lowfatcode/alright-fonts/tree/main/sample-fonts
+
 vector = PicoVector(display)
-vector.set_antialiasing(ANTIALIAS_NONE)
-result = vector.set_font('font_1.af', 50)  #vector.setfont("font-name-directory",font-size-int)
+vector.set_antialiasing(ANTIALIAS_X4)
+result = vector.set_font('IndieFlower-Regular.af', 50)  # font from https://github.com/lowfatcode/alright-fonts/tree/main/sample-fonts, stored in root on filesystem
 print(result)
+
 #SET PALETTE
 WHITE = display.create_pen(255,255,255)
-BLACK = display.create_pen(0,0,0)
+GREY = display.create_pen(20,20,20)
 
-#BLACK BG
-display.set_pen(BLACK)
+# BG
+display.set_pen(GREY)
 display.clear()
 
-#SET PEN AND DISPLAY TEXT - vector.text("TextHere",X,Y,ANGLEDEG)
+#SET PEN AND DISPLAY TEXT
 display.set_pen(WHITE)
 vector.text("Hello World!", 60, 90, 0)
 
