8b7ce1246ee6eb1e4c48cdaa0c1aad76882ec422
saliWd <57621907+saliWd@users.noreply.github.com>
2025-10-16 12:34:04 +0200

    different font, seems to work

--- pico2w/trial/font_trial_1.py
+++ pico2w/trial/font_trial_1.py
@@ -5,10 +5,13 @@ from picovector import PicoVector, ANTIALIAS_X4 # type: ignore
 #SET DISPLAY
 display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2)
 display.set_backlight(0.8)
+# font taken from
+# https://github.com/Gadgetoid/alright-fonts/blob/effb2fca35909a0f2aff7ed04b76c14286490817/sample-fonts/IndieFlower/IndieFlower-Regular.af
+# (https://github.com/lowfatcode/alright-fonts/tree/main/sample-fonts does not work, see https://forums.pimoroni.com/t/presto-and-alright-fonts-issue-picovector/28396/4)
 
 vector = PicoVector(display)
 vector.set_antialiasing(ANTIALIAS_X4)
-result = vector.set_font('IndieFlower-Regular.af', 50)  # font from https://github.com/lowfatcode/alright-fonts/tree/main/sample-fonts, stored in root on filesystem
+result = vector.set_font('IndieFlower-Regular.af', 50)  # font from , stored in root on filesystem
 print(result)
 
 #SET PALETTE
