1001c350a479c9df1204bfe8842192dfd28960f7
saliWd <57621907+saliWd@users.noreply.github.com>
2026-01-08 17:02:09 +0100

    preparation for 2026-values copy

--- web/tmp_verbrauch/copyOld.php
+++ web/tmp_verbrauch/copyOld.php
@@ -10,28 +10,31 @@ $dbConn = initialize();
 // - Winter: CON=0.2808, GEN=0.1200
 
 
-$sql = "SELECT * from `verbrauch` WHERE `zeit` < \"2025-12-31 23:59:59\" AND `copied` = 0 ORDER BY `id` DESC LIMIT 1"; 
+// $sql = "SELECT * from `verbrauch` WHERE `zeit` < \"2025-12-31 23:59:59\" AND `copied` = 0 ORDER BY `id` DESC LIMIT 1"; 
+$sql = 'SELECT * from `verbrauch` WHERE `userid` = "1" AND `copied` = 0 ORDER BY `id` DESC LIMIT 1';
 $result = $dbConn->query(query:$sql);
 $queryCount = $result->num_rows;
 
 if ($queryCount > 0) {
     $rowOld = $result->fetch_assoc();
     
+    /*
     if ($rowOld['consHtDiff']+$rowOld['consNtDiff'] > 0) { 
         $conRate = ($rowOld['consHtDiff']*0.3318 + $rowOld['consNtDiff']*0.2718) / ($rowOld['consHtDiff']+$rowOld['consNtDiff']);
     } else { // both are zero, it does not matter
        $conRate = 0.3318;
     }  
+    */
 
     $sql =  'INSERT INTO `verbrauch_26` (`userid`, `con`, `conDiff`, `conRate`, `gen`, `genDiff`, `genRate`, `zeit`, `zeitDiff`, `thin`) VALUES (';
     //                                    '23', '', '', '', '', '', '', current_timestamp(), '', '0');
     $sql .= '"'.$rowOld['userid'].'", ';
     $sql .= '"'.$rowOld['cons'].'", ';
     $sql .= '"'.$rowOld['consDiff'].'", ';
-    $sql .= "$conRate, ";
+    $sql .= "0.2808, "; // only winter data is copied // $sql .= "$conRate, ";
     $sql .= '"'.$rowOld['gen'].'", ';
     $sql .= '"'.$rowOld['genDiff'].'", ';
-    $sql .= '"0.15", ';
+    $sql .= '"0.12", ';
     $sql .= '"'.$rowOld['zeit'].'", ';
     $sql .= '"'.$rowOld['zeitDiff'].'", ';
     $sql .= '"'.$rowOld['thin'].'");';
